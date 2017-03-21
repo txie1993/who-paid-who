f48f6200decfecd4bd428f1acc7b9130e17a0e62
Tony Xie <tx248@nyu.edu>
2017-03-20 21:38:07 -0700

    broken firebase auth, needs updates

--- App.js
+++ App.js
@@ -1,63 +1,130 @@
+'use strict';
 import React from 'react';
 import {
-    StyleSheet,
-    Text,
-    Navigator,
-    TouchableHighlight,
-    Alert,
-    Button,
-    TextInput,
-    View,
-    ListView,
-    ScrollView,
-    Image
+  AppRegistry,
+  Text,
+  View,
+  Navigator,
+  AsyncStorage
 } from 'react-native';
 
+import Signup from './src/pages/signup';
+import Account from './src/pages/account';
+
+import Header from './src/components/header';
+
+import * as firebase from "firebase";
+var config = {
+  apiKey: "AIzaSyAxd5b1AEsiEQ1RoqPgyM7BatUMLAA0HJA",
+  authDomain: "who-paid-who.firebaseapp.com",
+  databaseURL: "https://who-paid-who.firebaseio.com",
+  storageBucket: "who-paid-who.appspot.com",
+  messagingSenderId: "706728769034"
+};
+const app = firebase.initializeApp(config);
+
+import styles from './src/styles/common-styles.js';
+
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        const ds = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2
+  constructor(props){
+    super(props);
+    this.state = {
+      component: null,
+      loaded: false
+    };
+  }
+
+  componentWillMount(){
+
+    AsyncStorage.getItem('user_data').then((user_data_json) => {
+
+      let user_data = JSON.parse(user_data_json);
+      let component = {component: Signup};
+      if(user_data != null){
+        app.authWithCustomToken(user_data.token, (error, authData) => {
+          if(error){
+            this.setState(component);
+          }else{
+            this.setState({component: Account});
+          }
         });
-        this.state = {
-            text: '',
-            dataSource: ds.cloneWithRows([
-                'John',
-                'Joel',
-                'James',
-                'Jimmy',
-                'Jackson',
-                'Jillian',
-                'Julie',
-                'Devin'
-            ])
-        };
-    }
-    render() {
-      const routes = [
-        {title: 'Dave', index: 0},
-        {title: 'Fuck Dave', index: 1},
-      ];
+      }else{
+        this.setState(component);
+      }
+    });
+
+  }
+
+  render(){
+
+    if(this.state.component){
       return (
         <Navigator
-  renderScene={(route, navigator) =>
-    <Text>Hello!</Text>
+          initialRoute={{component: this.state.component}}
+          configureScene={() => {
+            return Navigator.SceneConfigs.FloatFromRight;
+          }}
+          renderScene={(route, navigator) => {
+            if(route.component){
+              return React.createElement(route.component, { navigator });
+            }
+          }}
+        />
+      );
+    }else{
+      return (
+        <View style={styles.container}>
+          <Header text="React Native Firebase Auth" loaded={this.state.loaded} />
+          <View style={styles.body}></View>
+        </View>
+      );
+    }
+
   }
-  navigationBar={
-     <Navigator.NavigationBar
-       routeMapper={{
-         LeftButton: (route, navigator, index, navState) =>
-          { return (<Text>Cancel</Text>); },
-         RightButton: (route, navigator, index, navState) =>
-           { return (<Text>Done</Text>); },
-         Title: (route, navigator, index, navState) =>
-           { return (<Text>Awesome Nav Bar</Text>); },
-       }}
-       style={{backgroundColor: 'gray',
-       paddingTop: 100}}
-     />
-  }
-/>
+//     constructor(props) {
+//         super(props);
+//         const ds = new ListView.DataSource({
+//             rowHasChanged: (r1, r2) => r1 !== r2
+//         });
+//         this.state = {
+//             text: '',
+//             dataSource: ds.cloneWithRows([
+//                 'John',
+//                 'Joel',
+//                 'James',
+//                 'Jimmy',
+//                 'Jackson',
+//                 'Jillian',
+//                 'Julie',
+//                 'Devin'
+//             ])
+//         };
+//     }
+//     render() {
+//       const routes = [
+//         {title: 'Dave', index: 0},
+//         {title: 'Fuck Dave', index: 1},
+//       ];
+//       return (
+//         <Navigator
+//   renderScene={(route, navigator) =>
+//     <Text>Hello!</Text>
+//   }
+//   navigationBar={
+//      <Navigator.NavigationBar
+//        routeMapper={{
+//          LeftButton: (route, navigator, index, navState) =>
+//           { return (<Text>Cancel</Text>); },
+//          RightButton: (route, navigator, index, navState) =>
+//            { return (<Text>Done</Text>); },
+//          Title: (route, navigator, index, navState) =>
+//            { return (<Text>Awesome Nav Bar</Text>); },
+//        }}
+//        style={{backgroundColor: 'gray',
+//        paddingTop: 100}}
+//      />
+//   }
+// />
 // <Navigator
 //   initialRoute={routes[0]}
 //   initialRouteStack={routes}
@@ -74,8 +141,8 @@ export default class App extends React.Component {
 //   }
 //   style={{padding: 100}}
 // />
-      );
-    }
+    //   );
+    // }
 
     // render() {
     //     let pic = {
@@ -114,11 +181,11 @@ export default class App extends React.Component {
     // }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center'
+//     }
+// });
---
+++ src/components/button.js
@@ -0,0 +1,26 @@
+'use strict';
+import React from 'react';
+import{
+  AppRegistry,
+  Component,
+  Text,
+  View,
+  TouchableHighlight
+} from 'react-native';
+
+export default class button extends React.Component {
+
+  render(){
+    return (
+      <View>
+        <TouchableHighlight underlayColor={"#E8E8E8"} onPress={this.props.onpress} style={this.props.button_styles}>
+          <View>
+              <Text style={this.props.button_text_styles}>{this.props.text}</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('button', () => button);
